f4164da55e84b605e9055e4a7f217939e27c5d93
dripdropdr <workingsumin@gmail.com>
2023-05-27 12:11:46 +0900

    reduce classes

--- dataset.py
+++ dataset.py
@@ -11,8 +11,8 @@ class CustomImageDataset(Dataset):
         self.img_labels = pd.read_csv(labels)
         self.transform = transform
         self.target_transform = target_transform
-        self.label2id = {'angel': 0, 'apple': 1, 'arm': 2, 'banana': 3, 'baseball': 4, 'basketball': 5, 'bear': 6, 'beard': 7, 'bird': 8, 'birthday_cake': 9, 'book': 10, 'bowtie': 11, 'bread': 12, 'broccoli': 13, 'butterfly': 14, 'cake': 15, 'campfire': 16, 'candle': 17, 'carrot': 18, 'cat': 19, 'circle': 20, 'cloud': 21, 'coffee_cup': 22, 'cookie': 23, 'crown': 24, 'diamond': 25, 'dog': 26, 'dolphin': 27, 'donut': 28, 'duck': 29, 'ear': 30, 'envelope': 31, 'eye': 32, 'face': 33, 'fish': 34, 'flower': 35, 'foot': 36, 'frog': 37, 'garden': 38, 'hamburger': 39, 'hand': 40, 'headphones': 41, 'hourglass': 42, 'house': 43, 'house_plant': 44, 'ice_cream': 45, 'key': 46, 'laptop': 47, 'leaf': 48, 'light_bulb': 49, 'lightning': 50, 'lipstick': 51, 'lollipop': 52, 'megaphone': 53, 'microphone': 54, 'moon': 55, 'mountain': 56, 'mouse': 57, 'moustache': 58, 'mouth': 59, 'mushroom': 60, 'ocean': 61, 'octopus': 62, 'palm_tree': 63, 'panda': 64, 'pencil': 65, 'pizza': 66, 'rabbit': 67, 'rain': 68, 'rainbow': 69, 'shark': 70, 'sheep': 71, 'skull': 72, 'smiley_face': 73, 'snail': 74, 'snowflake': 75, 'snowman': 76, 'square': 77, 'squirrel': 78, 'star': 79, 'steak': 80, 'stop_sign': 81, 'strawberry': 82, 'sun': 83, 'teapot': 84, 'teddy-bear': 85, 'tree': 86, 'triangle': 87, 'watermelon': 88, 'whale': 89}
-        self.num_classes = 90
+        self.label2id = {'angel': 0, 'apple': 1, 'arm': 2, 'banana': 3, 'baseball': 4, 'basketball': 5, 'bear': 6, 'beard': 7, 'bird': 8, 'book': 9, 'bowtie': 10, 'bread': 11, 'butterfly': 12, 'cake': 13, 'campfire': 14, 'carrot': 15, 'cat': 16, 'cloud': 17, 'coffee_cup': 18, 'crown': 19, 'diamond': 20, 'dog': 21, 'donut': 22, 'eye': 23, 'face': 24, 'flower': 25, 'garden': 26, 'hand': 27, 'headphones': 28, 'house_plant': 29, 'ice_cream': 30, 'leaf': 31, 'light_bulb': 32, 'lightning': 33, 'ocean': 34, 'palm_tree': 35, 'pizza': 36, 'rabbit': 37, 'smiley_face': 38, 'snowflake': 39, 'snowman': 40, 'star': 41, 'strawberry': 42, 'sun': 43, 'teddy-bear': 44}
+        self.num_classes = 45
 
     def __len__(self):
         return len(self.img_labels)
@@ -22,7 +22,7 @@ class CustomImageDataset(Dataset):
         # image = read_image(img_path)
         image = Image.open(img_path, 'r')
         label = self.label2id[self.img_labels.iloc[idx, 1]]
-        one_hot = np.zeros(90)
+        one_hot = np.zeros(self.num_classes)
         one_hot[label] = 1
         
         if self.transform:
