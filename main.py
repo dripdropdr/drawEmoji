bc4827fa2f6e67c58847619dd5f7182e6fd8f7eb
dripdropdr <workingsumin@gmail.com>
2023-05-27 12:14:04 +0900

    emoji pop up function added

--- main.py
+++ main.py
@@ -4,16 +4,46 @@ import numpy as np
 import mediapipe as mp
 from collections import deque
 import torch
+from torchvision import datasets, models, transforms
+from copy import deepcopy
+from PIL import Image
+import os
+import random
 
-
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 # initialize classifier
+id2label = {0: 'angel', 1: 'apple', 2: 'arm', 3: 'banana', 4: 'baseball', 5: 'basketball', 6: 'bear', 7: 'beard', 8: 'bird', 9: 'book', 10: 'bowtie', 11: 'bread', 12: 'butterfly', 13: 'cake', 14: 'campfire', 15: 'carrot', 16: 'cat', 17: 'cloud', 18: 'coffee_cup', 19: 'crown', 20: 'diamond', 21: 'dog', 22: 'donut', 23: 'eye', 24: 'face', 25: 'flower', 26: 'garden', 27: 'hand', 28: 'headphones', 29: 'house_plant', 30: 'ice_cream', 31: 'leaf', 32: 'light_bulb', 33: 'lightning', 34: 'ocean', 35: 'palm_tree', 36: 'pizza', 37: 'rabbit', 38: 'smiley_face', 39: 'snowflake', 40: 'snowman', 41: 'star', 42: 'strawberry', 43: 'sun', 44: 'teddy-bear'}
 
+print('model loading ... ')
+device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
+model = models.resnet18(pretrained=True)
+model.conv1 = torch.nn.Conv2d(1, 64, 7, 2, 3)
+model.fc = torch.nn.Linear(model.fc.in_features, 45)
+model.to(device)
+checkpoint = torch.load('output/emoji/chekcpoint0022.pth')
+model.load_state_dict(checkpoint)
+model.eval()
+print('finish model load !!')
 
+input_size = 64
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(input_size),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(input_size),
+        transforms.ToTensor(),
+        # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
 
 # points, emoji & emoji location array
 points = [deque(maxlen=1024)]
 emoji = []
-emoji_loca = []
+emojiflg=False
 
 # index indicating deque in points, colors for lines
 index = 0
@@ -21,8 +51,10 @@ colors = (0, 0, 0)
 
 # Here is code for Canvas setup
 paintWindow = np.zeros((720,1280,3)) + 255
-paintWindow = cv2.rectangle(paintWindow, (40,1), (140,65), (0,0,0), 2)
-cv2.putText(paintWindow, "CLEAR", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+# paintWindow = cv2.rectangle(paintWindow, (40,1), (140,65), (0,0,0), 2)
+# paintWindow = cv2.rectangle(paintWindow, (160,1), (255,65), (125, 125, 125), -1)
+# cv2.putText(paintWindow, "CLEAR", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+# cv2.putText(paintWindow, "EMOJI", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 cv2.namedWindow('Paint', cv2.WINDOW_AUTOSIZE)
 
 # initialize mediapipe
@@ -37,7 +69,7 @@ while ret:
     # Read each frame from the webcam
     ret, frame = cap.read()
 
-    y, x, c = frame.shape
+    h, w, c = frame.shape
 
     # Flip the frame vertically
     frame = cv2.flip(frame, 1)
@@ -46,6 +78,12 @@ while ret:
     # CLEAR button 
     frame = cv2.rectangle(frame, (40,1), (140,65), (0,0,0), 2)
     cv2.putText(frame, "CLEAR", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+    # EMOJI button 
+    frame = cv2.rectangle(frame, (160,1), (255,65), (125, 125, 125), -1)
+    cv2.putText(frame, "EMOJI", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
+    frame = cv2.rectangle(frame, (275,1), (370,65), (0,0,0), 2)
+    cv2.putText(frame, "EMO CLEAR", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
 
     # Get hand landmark prediction
     result = hands.process(framergb)
@@ -55,8 +93,8 @@ while ret:
         landmarks = []
         for handslms in result.multi_hand_landmarks:
             for lm in handslms.landmark:
-                lmx = int(lm.x * x)
-                lmy = int(lm.y * y)
+                lmx = int(lm.x * w)
+                lmy = int(lm.y * h)
 
                 landmarks.append([lmx, lmy])
 
@@ -66,16 +104,28 @@ while ret:
         center = (landmarks[8][0],landmarks[8][1])
         middle = (landmarks[12][0],landmarks[12][1])
         cv2.circle(frame, center, 3, (0,255,0),-1)
-        print(center[1]-middle[1])
+
+        # Stop drawing
         if (middle[1]-center[1]<30):
             points.append(deque(maxlen=512))
             index += 1
-
+        # In button
         elif center[1] <= 65:
-            if 40 <= center[0] <= 140: # Clear Button
+            # clear button
+            if 40 <= center[0] <= 140: 
                 points = [deque(maxlen=512)]
                 index = 0
                 paintWindow[67:,:,:] = 255
+            # emoji button
+            elif 160 <= center[0] <= 255:
+                emojiflg = True
+                image = deepcopy(paintWindow)
+                points = [deque(maxlen=512)]
+                index = 0
+                paintWindow[67:,:,:] = 255
+            # emoji clear button
+            elif 275 <= center[0] <= 370:
+                emoji = []
         else :
             points[index].appendleft(center)
 
@@ -83,6 +133,12 @@ while ret:
     else:
         points.append(deque(maxlen=512))
         index += 1
+
+    # draw emoji
+    if len(emoji) > 0:
+        print(len(emoji))
+        for emo, y, x in emoji:
+            frame[y:y+emo.shape[0], x:x+emo.shape[1], :] = emo
 
     # Draw lines of all the colors on the canvas and frame
     for j in range(len(points)):
@@ -96,18 +152,28 @@ while ret:
     cv2.imshow("Paint", paintWindow)
 
 
-    # Classificate drawing
-    '''
-        paintedWindow : (h * w) numpy array.
-    '''
+    if emojiflg:
+        # Classificate drawing
+        '''
+            paintedWindow : (h * w) numpy array.
+        '''
+        # Crop paintedWindow to square.
+        image = image[:, int(w/2 - h/2):int(w/2 + h/2)]
+        image = Image.fromarray(np.uint8(image)).convert('L')
+        # image = np.expand_dims(image, axis=0)
 
-    # Crop paintedWindow to square.
-    image = paintWindow[:, int(x/2 - y/2):int(x/2 + y/2)]
+        # Put image to Classifier
+        
+        # image = Image.open("bus.jpg").convert("RGB")
+        image = data_transforms['val'](image)
 
-    # Put image to Classification.
-
-
-
+        # predict images
+        output = model(image[None])
+        pred = torch.argmax(output, axis=1)
+        emo_img = cv2.imread(f'emoji/{id2label[pred.item()]}.png')
+        emoji.append([emo_img, (random.randint(0, h - 100)), random.randint(0, w - 100)])
+        print(id2label[pred.item()], emoji)
+        emojiflg = False
 
     if cv2.waitKey(1) == ord('q'):
         break
